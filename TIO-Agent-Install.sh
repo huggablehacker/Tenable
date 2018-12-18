7d9df9c247436aea8c989afc4029c82d79e9a0cd
Huggable Hacker <huggablehacker@users.noreply.github.com>
2018-12-18 08:23:22 -0500

    Update TIO-Agent-Install.sh

--- TIO-Agent-Install.sh
+++ TIO-Agent-Install.sh
@@ -8,4 +8,4 @@ fi
 sudo /Library/NessusAgent/run/sbin/nessuscli agent link --key=00abcd00000efgh11111i0k222lmopq3333st4455u66v777777w88xy9999zabc00 --cloud --groups=$1
 
 #used to link a Tenable IO agent to your instance of TIO. Tested up to MacOS High Sierra.
-#make sue to change the permissions "chmod a+x"
+#make sure to change the permissions "chmod a+x"
